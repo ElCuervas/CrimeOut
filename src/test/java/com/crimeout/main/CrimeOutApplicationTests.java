b7f46256bb1a8d20470fb01ab62dcdffa8ace74f
ElCuervas <sebastianaillapan017@gmail.com>
2025-05-16 21:35:14 -0400

    Subida de archivos base para backend

---
+++ src/test/java/com/crimeout/main/CrimeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimeout.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
