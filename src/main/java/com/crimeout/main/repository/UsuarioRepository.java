e230018a1a389bfae0a3d812cce4de37b789479e
ElCuervas <sebastianaillapan017@gmail.com>
2025-05-17 18:43:56 -0400

    Feat: Añade el repositorio de Usuario

---
+++ src/main/java/com/crimeout/main/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.crimeout.main.repository;
+
+import com.crimeout.main.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+    Optional<Usuario> findByRut(String rut);
+}
